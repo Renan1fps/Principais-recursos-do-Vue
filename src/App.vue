40810b7ab500c627d045bc53d33daf4801977aeb
Renan Oliveira <renan.sergipe68@gmail.com>
2021-04-22 17:46:50 -0300

    Watch

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <template>
   <h1>{{ count }}</h1>
-  <button @click="incrementar">Incrementar</button>
-  <button @click="diminuir">Diminuir</button>
-  <button @click="resetar">Resetar</button>
+  <button @click="count++">Incrementar</button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, watch } from 'vue'
 
 export default defineComponent({
   data() {
@@ -14,15 +12,10 @@ export default defineComponent({
       count: 0
     }
   },
-  methods: {
-    incrementar() {
-      this.count++
-    },
-    diminuir() {
-      this.count--
-    },
-    resetar() {
-      this.count = 0
+  watch: {
+    count(newValue, oldValue) {
+      console.log(oldValue)
+      console.log(newValue)
     }
   }
 })
