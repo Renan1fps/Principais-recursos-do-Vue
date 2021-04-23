bfec4716f7c0ecbbaa2ca9c63f27ce99346cb723
Renan Oliveira <renan.sergipe68@gmail.com>
2021-04-23 18:31:03 -0300

    v-model

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,32 @@
 <template>
-  <ul>
-    <li v-for="product in products" :key="product.id">
-      {{ product.nome }}
-    </li>
-  </ul>
+  <form>
+    <input type="text" placeholder="Nome" v-model="name" />
+
+    <input type="number" placeholder="Idade" v-model.number="age" />
+
+    <textarea rows="2" placeholder="Descrição" v-model="message"></textarea>
+
+    <input type="checkbox" v-model="newsletter" /> Newsletter <br />
+
+    <input type="radio" name="color" value="vermelho" v-model="color" />
+    Vermelho
+    <input type="radio" name="color" value="verde" v-model="color" /> Verde
+    <input type="radio" name="color" value="azul" v-model="color" /> Azul
+
+    <select v-model="fruit">
+      <option disabled selected>Selecione uma fruta</option>
+      <option value="uva">Uva</option>
+      <option value="banana">Banna</option>
+      <option value="morango">Morango</option>
+    </select>
+  </form>
+
+  <p>Nome: {{ name }}</p>
+  <p>Idade: {{ age }}</p>
+  <p>Mensagem: {{ message }}</p>
+  <p>Newsletter: {{ newsletter }}</p>
+  <p>Cor: {{ color }}</p>
+  <p>Fruta: {{ fruit }}</p>
 </template>
 
 <script lang="ts">
@@ -12,14 +35,27 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   data() {
     return {
-      products: [
-        { id: 1, nome: 'feijão' },
-        { id: 2, nome: 'arroz' },
-        { id: 3, nome: 'batata' },
-        { id: 4, nome: 'carne' }
-      ]
+      name: '',
+      age: 0,
+      message: '',
+      newsletter: false,
+      color: '',
+      fruit: ''
     }
   }
 })
 </script>
 
+<style scoped>
+input[type='email'],
+input[type='text'],
+select,
+textarea {
+  display: block;
+}
+
+form {
+  padding-bottom: 1rem;
+  border-bottom: 2px solid #000;
+}
+</style>
