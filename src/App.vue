89db448dfc0dcdb66ec84d4c2f8bf14686f1f0c0
Renan Oliveira <renan.sergipe68@gmail.com>
2021-04-27 21:42:55 -0300

    Diretivas personalizadas 01

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,22 @@
 <template>
-  <form>
-    <input type="text" placeholder="Nome" v-model="name" />
-
-    <input type="number" placeholder="Idade" v-model.number="age" />
-
-    <textarea rows="2" placeholder="Descrição" v-model="message"></textarea>
-
-    <input type="checkbox" v-model="newsletter" /> Newsletter <br />
-
-    <input type="radio" name="color" value="vermelho" v-model="color" />
-    Vermelho
-    <input type="radio" name="color" value="verde" v-model="color" /> Verde
-    <input type="radio" name="color" value="azul" v-model="color" /> Azul
-
-    <select v-model="fruit">
-      <option disabled selected>Selecione uma fruta</option>
-      <option value="uva">Uva</option>
-      <option value="banana">Banna</option>
-      <option value="morango">Morango</option>
-    </select>
-  </form>
-
-  <p>Nome: {{ name }}</p>
-  <p>Idade: {{ age }}</p>
-  <p>Mensagem: {{ message }}</p>
-  <p>Newsletter: {{ newsletter }}</p>
-  <p>Cor: {{ color }}</p>
-  <p>Fruta: {{ fruit }}</p>
+  <div>
+    <h1 v-highlight="'red'"></h1>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  data() {
-    return {
-      name: '',
-      age: 0,
-      message: '',
-      newsletter: false,
-      color: '',
-      fruit: ''
+  directives: {
+    highlight: {
+      beforeMount(el: HTMLElement, biding){
+        console.log(el)
+      }
     }
   }
 })
 </script>
 
 <style scoped>
-input[type='email'],
-input[type='text'],
-select,
-textarea {
-  display: block;
-}
-
-form {
-  padding-bottom: 1rem;
-  border-bottom: 2px solid #000;
-}
 </style>
