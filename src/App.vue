9f0fe16b50337ae726ecd32ba26c07c6d64f7f1f
Renan Oliveira <renan.sergipe68@gmail.com>
2021-05-03 19:21:44 -0300

    Transições

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,33 @@
 <template>
- <ul>
-   <li @click="currentView='Home'">Home</li>
-   <li @click="currentView='Sobre'">Sobre</li>
-   <li @click="currentView='Contato'">Contato</li>
- </ul>
- <component :is="currentView"/>
+<div>
+  <button @click="show= !show">Toggle</button>
+  <transition name="fade">
+    <h1 v-if="show">Cataline</h1>
+  </transition>
+  
+</div>
+  
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import Home from './pages/Home.vue'
-import Sobre from './pages/Sobre.vue'
-import Contato from './pages/Contato.vue'
-
 
 export default defineComponent({
-  components: {
-    Sobre,
-    Home,
-    Contato 
-  },
-  data(){
-    return{
-      currentView:'Home'
+  data () {
+    return {
+      show: false
     }
   }
-  
 })
 </script>
+
+<style scoped>
+.fade-enter-from, .v-leave-to{
+opacity:0
+}
+.fade-enter-active, .v-leave-active{
+transition: opacity 3s
+}
+</style>
 
 
