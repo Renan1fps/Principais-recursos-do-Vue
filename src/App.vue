0d0c5c9d00b5632f6f376ec8d2c180754bc0f3f4
Renan Oliveira <renan.sergipe68@gmail.com>
2021-05-03 17:55:54 -0300

    Troca de elementos dinamica

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,31 @@
 <template>
-  <ButtonEvent text="Botão" @meuEvento="action($event)" />
+ <ul>
+   <li @click="currentView='Home'">Home</li>
+   <li @click="currentView='Sobre'">Sobre</li>
+   <li @click="currentView='Contato'">Contato</li>
+ </ul>
+ <component :is="currentView"/>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Home from './pages/Home.vue'
+import Sobre from './pages/Sobre.vue'
+import Contato from './pages/Contato.vue'
 
 
 export default defineComponent({
   components: {
-    
+    Sobre,
+    Home,
+    Contato 
   },
-  methods: {
-    action(event: string) {
-      alert(event)
+  data(){
+    return{
+      currentView:'Home'
     }
   }
+  
 })
 </script>
 
