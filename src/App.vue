1654e1e6b3a1f8fe4d251f5b1e5f9a6655108280
Renan Oliveira <renan.sergipe68@gmail.com>
2021-05-04 14:55:51 -0300

    Biblioteca de animações animate com transições

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
-<div>
-  <button @click="show= !show">Toggle</button>
-  <transition name="fade">
-    <h1 v-if="show">Cataline</h1>
-  </transition>
-  
-</div>
-  
+  <div>
+    <button @click="show = !show">Toggle</button>
+    <transition
+      name="fade"
+      enter-active-class="animate__animated animate__slideInLeft"
+      leave-active-class="animate__animated animate__hinge"
+    >
+      <h1 v-if="show">Cataline</h1>
+    </transition>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import 'animate.css'
 
 export default defineComponent({
-  data () {
+  data() {
     return {
       show: false
     }
@@ -22,12 +25,6 @@ export default defineComponent({
 </script>
 
 <style scoped>
-.fade-enter-from, .v-leave-to{
-opacity:0
-}
-.fade-enter-active, .v-leave-active{
-transition: opacity 3s
-}
 </style>
 
 
